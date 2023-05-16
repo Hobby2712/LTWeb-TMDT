25ee2d2a94b6bf516a050ac48ff60786d138c48e
gitdanh <thanhdanhnguyen46@gmail.com>
2023-05-16 11:10:14 +0700

    update add product api

--- src/main/java/Util/Constant.java
+++ src/main/java/Util/Constant.java
@@ -1,7 +1,7 @@
 package Util;
 
 public class Constant {
-	public static final String DIR = "D:\\HCMUTE\\LTAndroid\\PROJECT\\WEB_DUONG\\LTWeb-TMDT\\src\\main\\webapp";
-	//public static final String DIR = "D:\\cacbaitap\\gitDesktop\\LTWeb-TMDT\\src\\main\\webapp"; //Danh
+	//public static final String DIR = "D:\\HCMUTE\\LTAndroid\\PROJECT\\WEB_DUONG\\LTWeb-TMDT\\src\\main\\webapp";
+	public static final String DIR = "D:\\cacbaitap\\gitDesktop\\LTWeb-TMDT\\src\\main\\webapp"; //Danh
 
 }
