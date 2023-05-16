3453bc22333de5156fe89d117f7d8950d540c3ea
gitdanh <thanhdanhnguyen46@gmail.com>
2023-05-16 21:12:46 +0700

    update

--- src/main/java/APIResource/user/ForgetPasswordApi.java
+++ src/main/java/APIResource/user/ForgetPasswordApi.java
@@ -16,7 +16,7 @@ import com.google.gson.Gson;
 import DAO.UserDAO;
 import DaoImpl.UserDAOImpl;
 import Entity.User;
-import Entity.API.APIResponse;
+import Entity.api.APIResponse;
 
 @WebServlet(urlPatterns = { "/api/v1/forgotPassword/*" })
 public class ForgetPasswordApi extends HttpServlet {
